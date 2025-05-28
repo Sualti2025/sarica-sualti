768d9e307292d7f375922e5cc14a724c08b60077
kadiryesilyurt315 <154063779+kadiryesilyurt@users.noreply.github.com>
2025-05-28 18:26:26 +0300

    Update main.py
    main fonku şerit takiple güncellendi

--- ROV/main.py
+++ ROV/main.py
@@ -4,6 +4,7 @@ from mavlink_connection import connect, set_mode, arm_vehicle, disarm_vehicle
 from hareket_fonksiyonu import ileri_git, geri_git, saga_don, sola_don, saga_kay, sola_kay, yukari_cik, asagi_in, dur
 from imu_kontrol import request_imu_data, get_attitude
 from kamera_goruntu import baslat_kamera, goster_kare, kapat_kamera
+from serit_takip import serit_takip
 import cv2
 
 if __name__ == '__main__':
@@ -22,7 +23,15 @@ if __name__ == '__main__':
             while True:
                 get_attitude(master, log_file)
 
-                goster_kare(cap)  # Kameradan kare göster
+                ret, frame = cap.read()
+                if not ret:
+                    break
+                
+                # Şerit takibi uygula
+                islenmis = serit_takip(frame)
+
+                # İşlenmiş görüntüyü göster
+                cv2.imshow("Serit Takibi", islenmis)
 
                 saga_kay(master, 2)
 
